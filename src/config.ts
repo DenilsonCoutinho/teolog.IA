a74cb4625b8e33c84c1be7cd0884faf62c008db8
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2025-05-06 19:21:07 -0300

    alterando preços

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export const config = {
         publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
         secretKey: process.env.STRIPE_SECRET_KEY,
         plans: {
-            freePriceId: "price_1RJitfHt6s00L0BLyqxdieTR",
-            premiumPriceId: 'price_1RJkBiHt6s00L0BLdpydE3gV',
+            freePriceId: "price_1RLtiaHt6s00L0BLf5Gsl18q",
+            premiumPriceId: 'price_1RLtgFHt6s00L0BL26QKeUPq',
         }
         // price_1RIvC4Ht6s00L0BLY8TCkLm1
         //   webhookSecret: 'whsec_17a14e2622bead06a4132460bb6ea5a4a9b3a14e8c81f7cb7ebaa73f4cd770e5'
