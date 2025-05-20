7c6a5429df24fbba907bd82c34f2f4b487c7162d
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2025-05-19 22:09:26 -0300

    ajustes no authtrust

--- auth.ts
+++ auth.ts
@@ -5,7 +5,6 @@ import authConfig from "./auth.config"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  trustHost:true,
   session: {
     strategy: "jwt",
     maxAge: 60 * 60 * 60 ,
