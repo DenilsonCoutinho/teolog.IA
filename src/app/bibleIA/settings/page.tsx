3d19cf68226ccf9bcdd58c6807f2e42d27f28e20
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2025-06-03 23:02:30 -0300

    ajuste

--- src/app/bibleIA/settings/page.tsx
+++ src/app/bibleIA/settings/page.tsx
@@ -1,24 +1,8 @@
-
-import { typetheology, Typetheology, typetranslations } from "@prisma/client"
 import Settings from "./settings"
-import { cookies } from "next/headers"
-import { Suspense } from "react"
-// import SettingsWrapper from "./settingsWrapper"
-type Theology = {
-    data: {
-        type_theology: typetheology
-    }
-}
-
-type Translation = {
-    data: {
-        type_translations: typetranslations
-    }
-}
 
 export default async function SettingsServerSide() {
-   
+
     return (
-            <Settings   />
+        <Settings />
     )
 }
