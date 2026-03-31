108c97495bae93c244b1f2bd4e02c13247abdd04
Denilson coutinho <94764138+DenilsonCoutinho@users.noreply.github.com>
2026-03-31 13:20:40 -0300

    Update page.tsx

--- src/app/cancelado/page.tsx
+++ src/app/cancelado/page.tsx
@@ -2,7 +2,7 @@ export default function CanceladoPage() {
   return (
     <div className="h-screen w-full bg-white flex items-center justify-center">
       <h1 className="text-black text-3xl font-semibold">
-        Sua assinatura foi cancelada
+        Estamos encerrado nossas atividades devido a falta de usuários!
       </h1>
     </div>
   );
