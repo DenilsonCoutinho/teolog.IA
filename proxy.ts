7a43b5ba3d8b5704cf3ab55bc0f884edd6f357a0
Denilson coutinho <94764138+DenilsonCoutinho@users.noreply.github.com>
2026-03-31 12:43:32 -0300

    Add proxy function to handle request redirection

---
+++ proxy.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  // evita loop infinito
+  if (pathname === "/cancelado") {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/cancelado", request.url));
+}
+
+export const config = {
+  matcher: ["/((?!_next|favicon.ico).*)"],
+};
